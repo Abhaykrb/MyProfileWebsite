178e2db32403e5e0ded5334bbe99ae221d354299
Abhaykrb <65072557+Abhaykrb@users.noreply.github.com>
2020-07-17 06:00:13 -0700

    Add files via upload

---
+++ js/interact.js
@@ -0,0 +1,10 @@
+// for hamburg menu onclick work //
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
